3ddb2493218e3124555700297ae350d66c248e09
vthakur0905 <vthakur0905@gmail.com>
2025-04-03 19:07:31 +0530

    completed the signup route

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -31,15 +31,74 @@ export async function POST(request : Request){
             email
         })
 
+        const verifyCode = Math.floor(100000 + Math.random() * 900000).toString()
+
 
         if(existingUserByEmail) {
-            true // TODO: 
+            if(existingUserByEmail.isVerified){
+                return Response.json({
+                    success : false , 
+                    message : "user already exists with this email"
+                }, {
+                    status : 400
+                })
+            }
+            else {
+                const hashedPassword = await bcrypt.hash(password , 10) ; 
+                existingUserByEmail.password = hashedPassword ; 
+
+                existingUserByEmail.verifyCode = verifyCode ; 
+                existingUserByEmail.verifyCodeExpiry = new Date(Date.now() + 3600000) ; 
+
+                await existingUserByEmail.save() ;
+
+            }
         }
         else {
             const hashedPassword  = await bcrypt.hash(password, 10) ;
             const expiryDate = new Date();
             expiryDate.setHours(expiryDate.getHours() + 1) ;
+
+
+            const newUser = new UserModel({
+                username, 
+                email , 
+                password : hashedPassword , 
+                verifyCode, 
+                verifyCodeExpiry : expiryDate , 
+                isVerified : false , 
+                isAcceptingMessage : true , 
+                messages: []
+            })
+
+
+            await newUser.save() ;
+
         }
+
+        //send verification email 
+        const emailResponse = await sendVerificationEmail(
+            email, 
+            username, 
+            verifyCode
+        )
+
+        if(!emailResponse.success){
+            return Response.json({
+                success : false ,
+                message : emailResponse
+            }, {
+                status : 500
+            })
+        }
+
+
+        return Response.json({
+            success : true , 
+            message : "user registered successfully, please verify your email"
+        }, {
+            status : 201
+        })
 
 
     }
